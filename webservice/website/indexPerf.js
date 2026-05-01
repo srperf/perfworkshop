d61f3cfcecae39b0858635f2205f9e71f0e168ca
srperf <leandro@perfbytes.com>
2026-05-01 11:52:33 +0200

    Updated wesite site

--- webservice/website/indexPerf.js
+++ webservice/website/indexPerf.js
@@ -7,7 +7,7 @@ const homePage = `
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Selenium Test Page</title>
+    <title>Website Test Page</title>
     <style>
         body { font-family: Arial, sans-serif; padding: 20px; }
         #dynamicSection { margin-top: 20px; }
